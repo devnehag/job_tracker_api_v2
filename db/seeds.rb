127f2ed454f0fd5ccdd3e7078f51f3de9d5e19ad
devnehag <gupta.neha2004@gmail.com>
2024-03-20 20:43:24 -0700

    seeded data in tables using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,43 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Seed users
+10.times do
+  User.create!(
+    username: Faker::Internet.unique.username,
+    email: Faker::Internet.unique.email,
+    password: 'password' # You can set a default password for testing
+  )
+end
+
+# Seed jobs
+20.times do
+  Job.create!(
+    status: ['pending', 'completed', 'in progress'].sample,
+    user: User.all.sample,
+    job_title: Faker::Job.title,
+    company: Faker::Company.name,
+    job_description: Faker::Lorem.paragraph,
+    company_url: Faker::Internet.url,
+    date: Faker::Date.between(from: 1.year.ago, to: Date.today),
+    point_of_contact: Faker::Name.name,
+    job_reference: Faker::Alphanumeric.alphanumeric(number: 10),
+    tech_stack: Faker::ProgrammingLanguage.name
+  )
+end
+
+# Seed checklists
+30.times do
+  checklist = Checklist.create!(
+    title: Faker::Lorem.words(number: 3).join(' '),
+    details: Faker::Lorem.sentence,
+    job: Job.all.sample
+  )
+  
+  # Seed checklist items
+  5.times do
+    ChecklistItem.create!(
+      task: Faker::Lorem.sentence,
+      checklist: checklist
+    )
+  end
+end
